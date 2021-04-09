45dd9860f738906ad755f525ae19cfcb3de6a288
Cecilia Da Silva <ceciliaadasilva@hotmail.com>
2021-04-09 19:32:25 -0300

    first commit. Ejercitacion de javascript hecha hasta el 14 inclusive

---
+++ index.js
@@ -0,0 +1,195 @@
+// Ejercicio 1
+
+  // let nombre = prompt('¿Cuál es tu nombre?');
+  // let apellido = prompt('Cuál es tu apellido?');
+
+  // alert (`Hola ${nombre} ${apellido}, bienvenida a Ada`);
+
+// EJERCICIO 2
+
+ // let gusto1 = prompt ('¿Cuál es el primer gusto?');
+ // let gusto2 = prompt ('¿Cuál es el segundo gusto?');
+ // let gusto3 = prompt ('¿Cuál es el tercer gusto?');
+
+ // alert (`Aquí tiene su helado de ${gusto1}, ${gusto2} y ${gusto3}`);
+
+// EJERCICIO 3
+
+ // let nombre = prompt ('¿Cuál es tu nombre?');
+ // let apellido = prompt ('¿Cuál es tu apellido?');
+ // let edad = prompt ('¿Cuál es tu edad?');
+ // let nacionalidad = prompt ('¿Cuál es tu nacionalidad?');
+ // let dni = prompt ('¿Cuál es tu número de documento?');
+
+ // alert (`El nuevo perfil fue ingresado al sistema: nombre: ${nombre}, apellido: ${apellido}, edad: ${edad}, nacionalidad: ${nacionalidad}, número de documento: ${dni}`);
+
+
+//  EJERCICIO 4
+
+ // let playlist = prompt ('¿Cuál es el nombre de la playlist?');
+ // let cancion1 = prompt ('¿Cuál es la primer canción?');
+ // let cancion2 = prompt('¿Cuál es la segunda canción?');
+ // let cancion3 = prompt ('¿Cuál es la tercer canción?');
+  
+ //  alert (`Se ha creado la playlist ${playlist} con las canciones ${cancion1}, ${cancion2}, ${cancion3}`);
+
+//  EJERCICIO 5
+
+ // let calle = prompt ('¿Cuál es la calle?');
+ // let numero = prompt ('¿Cuál es el número?');
+ // let departamento = prompt ('¿Cuál es el departamento?');
+ // let codigoPostal = prompt ('¿Cuál es el código postal?');
+ // let ciudad = prompt ('¿Cúal es la ciudad?');
+ // let pais = prompt ('¿Cúal es el país?');
+  
+ // alert (`La dirección que ha ingresado es: ${calle} ${numero} ${departamento}, ${codigoPostal}, ${ciudad}, ${pais}`);
+
+//  EJERCICIO 6
+ // Necesitamos un programa que pida ingresar una edad con el siguiente mensaje:
+ // Ingresá una edad por favor
+ // El programa deberá mostrar el equivalente de esa edad en años perro mediante el siguiente mensaje 
+ // El equivalente de {edad} en años perros es: {resultado}
+
+  // let edad = prompt ('Ingresá una edad por favor');
+
+  // let resultado = parseInt(edad) * 7;
+
+  // alert (`El equivalente de ${edad} en años perros es: ${resultado}`);
+
+// EJERCICO 7
+ // Necesitamos un programa que pida una cantidad de minutos con el siguiente mensaje:
+ // Ingresá una cantidad de minutos.
+ // Con esta información, el programa deberá mostrar el resultado de la conversión en 
+ // segundos mediante el mensaje: El resultado de la conversión de {minutos} minutos a segundos es: {resultado}
+
+  // let minutos = prompt ('Ingresá una cantidad de minutos');
+
+  // let resultado = parseInt(minutos) * 60
+
+  // alert (`El resultado de la conversión de ${minutos} minutos a segundos es: ${resultado}`);
+
+// EJERCICIO 8
+ // Necesitamos un programa que pida una cantidad de días con el siguiente mensaje:
+ // Ingresá una cantidad de días por favor
+ // El programa deberá mostrar el resultado de la conversión de los días a segundos mediante el mensaje: 
+ // El resultado de la conversión de {dias} días a segundos es: {resultado}
+
+  // let dias = prompt ('Ingresá una cantidad de días por favor');
+
+  // let resultado = (parseInt(dias) * 24) * 60 * 60;
+
+  // alert (`El resultado de la conversión de ${dias} días a segundos es: ${resultado}`);
+
+// EJERCICIO 9
+
+ // Necesitamos un programa que pida una cantidad de kilómetros mediante el mensaje:
+ // Ingresá una cantidad de kilómetros por favor
+ // Con esta información, el programa deberá mostrar el resultado de la conversión de kilómetros a millas 
+ // mediante el siguiente mensaje: El resultado de la conversión de {kilometros} kilometros a millas es: {resultado}
+
+  //  let kilometros = prompt ('Ingresá una cantidad de kilómetros por favor');
+
+  //  let resultado = parseInt(kilometros) * 0.621371;
+
+  //  alert (`El resultado de la conversión de ${kilometros} kilometros a millas es: ${resultado}`);
+
+// EJERCICIO 10
+ // Necesitamos un programa que pida ingresar el valor de la base y el valor de la altura de un triángulo mediante los siguientes mensajes:
+ // Ingresá el valor de la base del triángulo
+ // Ingresá el valor de la altura del triángulo
+ // Con esta información, el programa deberá mostrar el resultado del área del triángulo mediante el siguiente mensaje: 
+ // El resultado del área del triángulo con base {base} y altura {altura} es: {resultado}
+
+  //  let base = prompt ('Ingresá el valor de la base del tríangulo');
+  //  let altura = prompt ('Ingrese el valor de la altura del tríangulo');
+
+  //  let resultado = parseInt(base) * parseInt(altura) / 2;
+
+  //  alert (`El resultado del área del triángulo con base ${base} y altura ${altura} es: ${resultado}`);
+
+// EJERCICIO 11
+ // Necesitamos un programa que pida ingresar el valor de la base y el valor de la altura de un rectángulo mediante los siguientes mensajes:
+ // Ingresá el valor de la base del rectángulo
+ // Ingresá el valor de la altura del rectángulo
+ // Con esta información, el programa deberá mostrar el resultado del perímetro del rectángulo mediante el siguiente mensaje: 
+ // El resultado del perímetro del rectángulo con base {base} y altura {altura} es: {resultado}
+
+  //  let base = prompt ('Ingresá el valor de la base del rectángulo');
+  //  let altura = prompt ('Ingresá el valor de la altura del rectángulo');
+
+  //  let resultado = 2 * parseInt(base) + 2 * parseInt(altura);
+
+  //  alert (`El resultado del perímetro del rectángulo con base ${base} y altura ${altura} es: ${resultado}`);
+
+ // EJERCICIO 12
+ // Necesitamos un programa que pida ingresar un número, y luego el porcentaje que se desea obtener del mismo mediante los siguientes mensajes:
+ // Ingresá un número
+ // Ingresá un porcentaje
+ // Con esta información, el programa deberá mostrar el resultado del porcentaje mediante el siguiente mensaje: 
+ // El porcentaje {porcentaje} del número {numero} es: {resultado}
+
+  //  let numero = prompt ('Ingresá un número');
+  //  let porcentaje = prompt ('Ingresá un porcentaje');
+
+  //  let resultado = parseInt(numero) * parseInt(porcentaje) / 100 ;
+
+  //  alert (`El porcentaje ${porcentaje} del número ${numero} es: ${resultado}`);
+
+// EJERCICIO 13
+ // Necesitamos un programa que pida ingresar la distancia de un recorrido, mediante el mensaje:
+ // Ingresá la distancia del recorrido
+ // Con esta información, el programa deberá calcular cuánto tiempo tardaría en completar el recorrido en distintos medios de transporte 
+ // y luego mostrarlo mediante el siguiente mensaje: Para la distancia {distancia} km en bicicleta el tiempo de viaje es {resultadoEnBicicleta} hora/s, a pie {resultadoAPie} hora/s y en auto {resultadoEnAuto} hora/s
+
+ // Las velocidades de los medio de transporte son:
+ // a pie : 5 km/ hs
+ // bicicleta : 10 km/ hs
+ // auto : 50 km/hs
+
+//  let distancia = prompt ('Ingresá la distancia del recorrido');
+
+ //  let resultadoEnBicicleta = parseInt(distancia) / 10;
+ //  let resultadoAPie = parseInt(distancia) / 5;
+ //  let resultadoEnAuto = parseInt(distancia) / 50;
+
+ //  alert (`Para la distancia ${distancia} km en bicicleta el tiempo de viaje es ${resultadoEnBicicleta} hora/s, a pie ${resultadoAPie} hora/s y en auto ${resultadoEnAuto} hora/s`);
+
+// EJERCICIO 14
+ // Necesitamos un programa que pida ingresar 3 destinos de las escalas de un vuelo, y la duración de cada uno de ellos, mediante los siguientes mensajes:
+
+ // ¿Cúal es la primer escala?
+ // ¿Cúal es la duración de la primer escala?
+ // ¿Cúal es la segunda escala?
+ // ¿Cúal es la duración de la segunda escala?
+ // ¿Cúal es la tercer escala?
+ // ¿Cúal es la duración de la tercer escala?
+ // Con esta información, el programa deberá mostrar todas las escalas con su duración y la duración total del vuelo con el siguiente mensaje: 
+ // La duración total del vuelo con escala en {escalaUno} de duración {duracionEscalaUno} hs, {escalaDos} de duración {duracionEscalaDos} hs y {escalaTres} de duración {duracionEscalaTres} hs es: {resultado} hs.
+
+  //  let escalaUno = prompt ('¿Cúal es la primer escala?');
+  //  let duracionEscalaUno = prompt ('¿Cúal es la duración de la primer escala?');
+  //  let escalaDos = prompt ('¿Cúal es la segunda escala?');
+  //  let duracionEscalaDos = prompt ('¿Cúal es la duración de la segunda escala?');
+  //  let escalaTres = prompt ('¿Cúal es la tercer escala?');
+  //  let duracionEscalaTres = prompt ('¿Cúal es la duración de la tercer escala?');
+
+  //  let resultado = parseInt(duracionEscalaUno) + parseInt(duracionEscalaDos) + parseInt(duracionEscalaTres);
+
+ //  alert (`La duración total del vuelo con escala en ${escalaUno} de duración ${duracionEscalaUno} hs, ${escalaDos} de duración ${duracionEscalaDos} hs y ${escalaTres} de duración ${duracionEscalaTres} hs es: ${resultado} hs.`);
+
+// EJERCICIO 15
+ // Necesitamos un programa que pida ingresar un número de partida y una cantidad, mediante los siguientes mensajes:
+ // Ingresá un número
+ // Ingresá una cantidad
+ // Con esta información, el programa deberá incrementar el número cinco veces la cantidad y mostrar cinco mensajes 
+ // seguidos que muestren el número del mensaje y el total incrementado hasta el momento. 
+ // Por ejemplo si ingresamos el 6 como número y el 1 como cantidad, debería mostrar lo siguiente:
+
+ // 'Incremento 1: 7'
+ // 'Incremento 2: 8'
+ // 'Incremento 3: 9'
+ // 'Incremento 4: 10'
+ // 'Incremento 5: 11'
+
+//  let numero = prompt ('Ingresá un número');
+//  let cantidad = prompt ('Ingresá una cantidad');
